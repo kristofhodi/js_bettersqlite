8f56a507406ec1ceb82b56ec3c2144e8d50bc3fc
kristofhodi <144095755+kristofhodi@users.noreply.github.com>
2025-05-16 13:36:42 +0200

    Add files via upload

---
+++ database.js
@@ -0,0 +1,17 @@
+import Database from "better-sqlite3"
+
+const db = new Database('./data/database.sqlite')
+
+db.prepare(`CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING, age INTEGER)`).run()
+
+export const getUsers = () => db.prepare(`SELECT * FROM users`).all()
+export const getUser = () => db.prepare(`SELECT * FROM users WHERE id = ?`).get(id)
+export const saveUser = (name, age) => db.prepare(`INSERT INTO users (name, age) VALUES (?,?)`).run(name, age)
+export const updateUser = (id, name, age) => db.prepare(`UPDATE users SET name = ?, age = ?. WHERE id = ?`).run(name, age, id)
+export const deleteUser = (id) => db.prepare(`DELETE FROM users WHERE id = ?`).run(id)
+
+const users= [
+    {name: 'anna', age: 18},
+    {name: 'bob', age: 19}
+]
+//for (const user of users) saveUser(user.name, user.age)
